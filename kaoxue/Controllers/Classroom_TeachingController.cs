b41689710e6e7df031e3d8f96d0d97e37c74399a
夏令 <760941398@qq.com>
2016-11-23 09:14:32 +0800

    完善页面

---
+++ kaoxue/Controllers/Classroom_TeachingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kaoxue.Controllers
+{
+    public class Classroom_TeachingController : Controller
+    {
+        //
+        // GET: /Classroom_Teaching/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
