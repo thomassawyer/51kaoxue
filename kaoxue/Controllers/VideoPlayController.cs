6d120ec6e1dbcd30b6b5224d673dbd169232a46e
夏令 <760941398@qq.com>
2016-11-28 13:53:44 +0800

    视频详情页

---
+++ kaoxue/Controllers/VideoPlayController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kaoxue.Controllers
+{
+    public class VideoPlayController : Controller
+    {
+        //
+        // GET: /VideoPlay/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
