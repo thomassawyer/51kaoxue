d138c1ea105fe71b1fe930bff1070935aee1ab6d
夏令 <760941398@qq.com>
2016-12-02 17:31:12 +0800

    完善页面

---
+++ kaoxue/CustomScripts/advise.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../Scripts/jquery-1.8.2.js" />
+
+//url参数集合
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = decodeURI(window.location.search).substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+
+//点击span时，改变span样式
+function change_css_span(obj,css_class) {
+    $(obj).parent().children().each(function () {
+        $(this).removeClass(css_class);
+    });
+    $(obj).addClass(css_class);
+}
+
+$(document).ready(function () {
+
+});
